f6696b91ff0c8b3544a378422e54f765ed4b7d84
minsu0717 <alstnalstnalstnrlaalstn@gmail.com>
2023-08-08 13:09:23 +0900

    add : files for initial settings

--- minsukim/app.js
+++ minsukim/app.js
@@ -1,18 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const morgan = require("morgan");
+
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 const { DataSource } = require("typeorm");
 
-const myDataSource = new DataSource({
-  type: process.env.TYPEORM_CONNECTION,
-  host: process.env.TYPEORM_HOST,
-  port: process.env.TYPEORM_PORT,
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
+const appDataSource = new DataSource({
+  type: process.env.DB_CONNECTION,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
-myDataSource.initialize().then(() => {
-  console.log("Data Source has been initialized!");
+appDataSource
+  .initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((error) => console.log(error));
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/ping", function (req, res, next) {
+  res.json({ message: "pong" });
+});
+
+app.listen(3000, function () {
+  "listening on port 3000";
 });
