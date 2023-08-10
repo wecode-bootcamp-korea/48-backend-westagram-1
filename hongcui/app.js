87ce2b1fc9ed094ec017685fd7015b2bdc3e48de
홍이 <cuihong928@gmail.com>
2023-08-10 21:41:43 +0900

    [최홍] Assignment 2: Express API - 유저 회원가입 하기

--- hongcui/app.js
+++ hongcui/app.js
@@ -6,6 +6,8 @@ const morgan = require("morgan");
 
 const { DataSource } = require("typeorm");
 
+const bcrypt = require("bcrypt");
+
 const DataSource_id = new DataSource({
   type: process.env.DB_CONNECTION,
   host: process.env.DB_HOST,
@@ -21,9 +23,37 @@ DataSource_id.initialize().then(() => {
 const app = express();
 app.use(cors());
 app.use(morgan("combined"));
+app.use(express.json());
 app.get("/ping", function (req, res, next) {
   res.json({ message: "pong" });
 });
+
+app.post("/users", async (req, res) => {
+  const { user_id, password, email } = req.body;
+  const saltRounds = 12;
+  const hash_password = bcrypt.hashSync(password, saltRounds);
+
+  await DataSource_id.query(
+    "INSERT INTO users(user_id, password, email ) VALUES (?, ?, ?);",
+    [user_id, hash_password, email]
+  );
+  res.status(201).json({ message: "successfully created" });
+});
+
+// main();
+
+// app.get("/users", async (req, res) => {
+//   await myDataSource.query(
+//     `SELECT
+//           users.user_id,
+//           users.password,
+//           users.email
+//       FROM users`,
+//     (err, rows) => {
+//       res.status(200).json(rows);
+//     }
+//   );
+// });
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
