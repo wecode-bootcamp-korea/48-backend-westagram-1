407a08a1f90e9b15de6c137a4194b2b37a1bb328
홍이 <cuihong928@gmail.com>
2023-08-10 11:12:17 +0900

    수정완료

--- hongcui/app.js
+++ hongcui/app.js
@@ -1,19 +1,21 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+
 const morgan = require("morgan");
 
 const { DataSource } = require("typeorm");
 
-const myDataSource = new DataSource({
-  type: process.env.TYPEORM_CONNECTION,
-  host: process.env.TYPEORM_HOST,
-  port: process.env.TYPEORM_PORT,
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
+const DataSource_id = new DataSource({
+  type: process.env.DB_CONNECTION,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
-myDataSource.initialize().then(() => {
+
+DataSource_id.initialize().then(() => {
   console.log("Data Source has been initialized!");
 });
 const app = express();
@@ -22,6 +24,7 @@ app.use(morgan("combined"));
 app.get("/ping", function (req, res, next) {
   res.json({ message: "pong" });
 });
+
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
